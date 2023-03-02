56c8b0117fb3a4740d14d68ab1099ffcd4ac11f4
rrbeech <autumnartist@gmail.com>
2023-03-02 08:43:58 -0500

    Scene Loader working

--- The-Wood-Between-Worlds/Assets/Scripts/Lift.cs
+++ The-Wood-Between-Worlds/Assets/Scripts/Lift.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.SceneManagement;
 
 
 public class Lift : MonoBehaviour
 {
-    [SerializeField] private Animator drop = null;
+    private Animator drop = null;
+    private bool alreadyTriggered = false;  // this is so that triggering happes only once.                                        
+    private const string sceneName = "Transition Scene";  //Scene name we are going to.
+    private IEnumerator coroutine;
+
+    // time after this script initializes, in seconds,
+    // that the scene transition will happen
+    private const float TIME_LIMIT = 2F; //seconds
 
     private void Awake()
     {
@@ -15,12 +23,22 @@ public class Lift : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("OnTriggerEnter occured");
-        if (other.CompareTag("Player"))
+        //Debug.Log("OnTriggerEnter occured");
+        if (other.CompareTag("Player") && alreadyTriggered == false) //if this is the first time the lift has been triggered...
         {
-            Debug.Log("Playing animation");
-            drop.Play("Down", 0, 0.0f);
-        }
+            //Debug.Log("Playing animation NOW");
+            drop.Play("goDown", 0, 0.0f);
+            alreadyTriggered = true; //set flag so we don't drop a second time.
 
+            coroutine = SceneLoader();
+            StartCoroutine(coroutine);
+        }
+    }
+
+
+    private IEnumerator SceneLoader()
+    {
+            yield return new WaitForSeconds(TIME_LIMIT);
+            SceneManager.LoadScene(sceneName);
     }
 }
